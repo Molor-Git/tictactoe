6deec548979765eb01334f2b8f4e3b50a91bc709
Molor-Git <mongolhuu0731@gmail.com>
2021-10-16 22:53:57 -0400

    Initial commit

---
+++ game.py
@@ -0,0 +1,29 @@
+
+# Create board
+# choose an initial player
+# Until someone wins, check for winner
+#   show the board
+#   choose location, mark it
+#   toggle active player
+
+# Game over, active payer won!
+
+def main():
+    # Board is a list of rows
+    # Rows
+    board = [
+        [None, None, None],
+        [None, None, None],
+        [None, None, None],
+
+    ]
+    board.get("1,2")
+    board[0]
+
+    board = {
+        "0,0":
+    }
+
+
+if __name__ == '__main__':
+    main()
